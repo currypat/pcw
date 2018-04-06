64ba2377f2c9fba765346088f43c5d56b13e34f2
patcurry <currypat1985@gmail.com>
2018-04-06 17:11:14 +0200

    trying to add a template tag for json stuff

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,41 @@
 # patcurryworks.com/blog/views.py
+from django.views import View
 from django.shortcuts import render
 
 from blog.models import Post
 
 def index(request):
     post_list = Post.objects.all()
-    context = {"post_list": post_list}
+
+    props = {
+        'users': [
+            {'username': 'alice'},
+            {'username': 'bob'},
+        ]
+    }
+
+
+    context = {
+        'post_list': post_list,
+        'props':props
+    }
     response = render(request, 'blog/index.html', context)
     return response
+
+#class Index(View):
+#    def get(self, request):
+#        props = {
+#            'users': [
+#                {'username': 'alice'},
+#                {'username': 'bob'},
+#            ]
+#        }
+#
+#        post_list = Post.objects.all()
+#
+#        context = {
+#            'props': props,
+#            'post_list': post_list
+#        }
+#
+#        render(request, 'blog/index.html', context)
