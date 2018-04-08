ffbb84e3faffdfec2d2a780ecf3d856e74505f17
currypat <currypat1985@gmail.com>
2018-04-08 14:01:03 +0200

    getting a little api going

--- exercise_api/views.py
+++ exercise_api/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from exercise_api.models import Exercise
+from exercise_api.serializers import ExerciseSerializer
 
-# Create your views here.
+
+@csrf_exempt
+def exercise_list(request):
+    """
+    List all code exercises, or create a new exercises.
+    """
+    if request.method == 'GET':
+        exercises = Exercise.objects.all()
+        serializer = ExerciseSerializer(exercises, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ExerciseSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def exercise_detail(request, pk):
+    """
+    Retrieve, update or delete a code exercise.
+    """
+    try:
+        exercise = Exercise.objects.get(pk=pk)
+    except Exercise.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ExerciseSerializer(exercise)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ExerciseSerializer(Exercise, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        exercise.delete()
+        return HttpResponse(status=204)
