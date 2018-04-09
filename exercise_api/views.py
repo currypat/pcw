3d013d8bdb7f45e5f8c7c760343ba41fcce2ccf3
currypat <currypat1985@gmail.com>
2018-04-09 17:47:34 +0200

    up to class based views in django rest framework tutorial

--- exercise_api/views.py
+++ exercise_api/views.py
@@ -1,52 +1,14 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+# patcurryworks.com/exercise_api/views.py
 from exercise_api.models import Exercise
 from exercise_api.serializers import ExerciseSerializer
+from rest_framework import generics
 
 
-@csrf_exempt
-def exercise_list(request):
-    """
-    List all code exercises, or create a new exercises.
-    """
-    if request.method == 'GET':
-        exercises = Exercise.objects.all()
-        serializer = ExerciseSerializer(exercises, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ExerciseSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+class ExerciseList(generics.ListCreateAPIView):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
 
 
-@csrf_exempt
-def exercise_detail(request, pk):
-    """
-    Retrieve, update or delete a code exercise.
-    """
-    try:
-        exercise = Exercise.objects.get(pk=pk)
-    except Exercise.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = ExerciseSerializer(exercise)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ExerciseSerializer(Exercise, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        exercise.delete()
-        return HttpResponse(status=204)
+class ExerciseDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
