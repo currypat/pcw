d0db3c6075ca8c11d3b1268564f7088f7d9187da
patcurry <currypat1985@gmail.com>
2018-04-09 16:37:51 +0200

    created a post detail page and moved blog index to post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,18 @@
 # patcurryworks.com/blog/views.py
 from django.core import serializers
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
-def index(request):
+def post_list(request):
     post_list = Post.objects.all()
+    context = {'post_list': post_list}
+    response = render(request, 'blog/post_list.html', context)
+    return response
 
-    post_list_serialized = serializers.serialize('json',
-                                                 post_list,
-                                                 fields=("text")) 
-
-    props = {
-        'users': [
-            {'username': 'alice'},
-            {'username': 'bob'},
-        ]
-    }
-
-
-    context = {
-        'post_list': post_list,
-        'post_list_serialized': post_list_serialized,
-        'props':props
-    }
-    response = render(request, 'blog/index.html', context)
+def post_detail(request, post_slug):
+    post = get_object_or_404(Post, post_slug=post_slug)
+    context = {'post': post}
+    response = render(request, 'blog/post_detail.html', context)
     return response
--- tests/blog/tests.py
+++ tests/blog/tests.py
@@ -2,33 +2,52 @@
 from django.test import TestCase
 from django.urls import resolve
 
-from blog.views import index
+from blog.views import post_list, post_detail
 from blog.models import Post
 
-class BlogIndexViewTests(TestCase):
+class PostListViewTests(TestCase):
 
     def setUp(self):
         self.blog_url = '/blog/'
-        self.post1 = Post.objects.create(text='first post')
-        self.post2 = Post.objects.create(text='second post')
-        self.post3 = Post.objects.create(text='third post')
+        self.post1 = Post.objects.create(title='post one', text='first post')
+        self.post2 = Post.objects.create(title='post two', text='second post')
 
-
-
-    def test_blog_index_resolves_to_blog_index_view(self):
+    def test_post_list_resolves_to_post_list_view(self):
         found = resolve(self.blog_url)
-        self.assertEqual(found.func, index)
+        self.assertEqual(found.func, post_list)
 
-    def test_blog_index_returns_200_status_code(self):
+    def test_post_list_returns_200_status_code(self):
         response = self.client.get(self.blog_url)
         self.assertEqual(response.status_code, 200)
 
-    def test_blog_index_view_has_Blog_in_it(self):
+    def test_post_list_view_has_Blog_in_it(self):
         response = self.client.get(self.blog_url)
         self.assertIn('Blog', response.content.decode('utf-8'))
 
-    def test_blog_index_view_returns_list_of_blog_posts(self):
+    def test_post_list_view_returns_list_of_blog_posts(self):
         response = self.client.get(self.blog_url)
+        self.assertIn('post one', response.content.decode('utf-8'))
+        self.assertIn('post two', response.content.decode('utf-8'))
+
+
+class PostDetailViewTests(TestCase):
+    
+    def setUp(self):
+        self.blog_url = '/blog/'
+        self.post1 = Post.objects.create(title='post one', text='first post')
+
+    def test_post_detail_resolves_to_post_detail_view(self):
+        found = resolve(self.blog_url + 'post-one/')
+        self.assertEqual(found.func, post_detail)
+
+    def test_post_detail_returns_200_status_code(self):
+        response = self.client.get(self.blog_url + 'post-one/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_detail_has_post_title_in_content(self):
+        response = self.client.get(self.blog_url + 'post-one/')
+        self.assertIn('post one', response.content.decode('utf-8'))
+
+    def test_post_detail_has_post_text_in_content(self):
+        response = self.client.get(self.blog_url + 'post-one/')
         self.assertIn('first post', response.content.decode('utf-8'))
-        self.assertIn('second post', response.content.decode('utf-8'))
-        self.assertIn('third post', response.content.decode('utf-8'))
