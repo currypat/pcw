7a845277216cd8d3aecfd2a6dc7d0cefd85ad352
patcurry <currypat1985@gmail.com>
2018-04-10 09:38:12 +0200

    setting up workouts models

--- workouts/models.py
+++ workouts/models.py
@@ -1,6 +1,17 @@
 # patcurryworks.com/workouts/models.py
 from django.db import models
+from django.utils.text import slugify
 
+"""
+https://www.medicinenet.com/weight_lifting/article.htm#how_do_i_go_about_lifting_for_tone_and_endurance
+Sets and reps are the terms used to describe the number of times you perform an exercise. A rep is the number of times you perform a specific exercise, and a set is the number of cycles of reps that you complete. For example, suppose you complete 15 reps of a bench press. You would say you've completed "one set of 15 reps." A set can be any number of reps, so if you complete 10 reps of a bench press, you would say you've completed "one set of 10 reps," and if you complete just five reps, then that would be "one set of five reps."
+"""
+
+
+# Session
+# Set
+# Rep
+# Exercise
 
 SECONDS = 'sec'
 REPETITIONS = 'reps'
@@ -10,20 +21,53 @@ UNIT_CHOICES = (
 )
 
 
-class Workout(models.Model):
-    """Workouts"""
-    created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100, blank=True)
-    amount = models.IntegerField(default=1)
-    sets = models.IntegerField(default=1)
+class Exercise(models.Model):
+    """This is the smallest individual unit."""
+    title = models.CharField(max_length=50, blank=True, null=True)
+    exercise_slug = models.SlugField(max_length=50, blank=True, null=True)
+
+
+
+    def save(self, *args, **kwargs):
+        self.exercise_slug = slugify(self.title)
+        super(Exercise, self).save(*args, **kwargs)
+
+class Set(models.Model):
+    """This set can have many repetitions of one exercise."""
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1, blank=True, null=True)
     units = models.CharField(
         max_length=4,
         choices=UNIT_CHOICES,
         default=REPETITIONS
     )
 
-    class Meta:
-        ordering = ('created',)
+class Session(models.Model):
+    """This session can have many sets of different types."""
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True)
+    sets = models.ForeignKey(Set, on_delete_models.CASCADE)
+
+
+
+#    amount = models.IntegerField(default=1)
+#    sets = models.IntegerField(default=1)
+#    units = models.CharField(
+#        max_length=4,
+#        choices=UNIT_CHOICES,
+#        default=REPETITIONS
+#    )
+
+#    class Meta:
+#        ordering = ('created',)
         
 
+#class Set(models.Model):
+#    """This set can have many exercises."""
 
+#class Exercise(models.Model):
+#    """This is the number of times a particular exercise was performed during a set.
+#    It should probably be called reps or something, and not Exercise. Exercise might
+#    be actually the individual exercise.
+#    """
+    
