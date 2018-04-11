75c5a4c9dceafee53270bb153da325bc25167310
currypat <currypat1985@gmail.com>
2018-04-11 18:54:41 +0200

    many to many displayed on session s

--- workouts/serializers.py
+++ workouts/serializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
 from workouts.models import Exercise, Set, Session, UNIT_CHOICES
 
+
 class ExerciseSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Exercise
         fields = ('title', 'exercise_slug')
 
         
 class SetSerializer(serializers.ModelSerializer):
-    # How do I make this list the name of the exercise and not the pk?
     exercise = serializers.SlugRelatedField(
         read_only=True,
         many=False,
