cd5b6b8c4072c9395b0eb335882193b779d5de5a
currypat <currypat1985@gmail.com>
2018-04-11 18:52:57 +0200

    many to many displayed on session stuff

--- workouts/serializers.py
+++ workouts/serializers.py
@@ -8,12 +8,19 @@ class ExerciseSerializer(serializers.ModelSerializer):
 
         
 class SetSerializer(serializers.ModelSerializer):
+    # How do I make this list the name of the exercise and not the pk?
+    exercise = serializers.SlugRelatedField(
+        read_only=True,
+        many=False,
+        slug_field='title')
+
     class Meta:
         model = Set
         fields = ('exercise', 'amount', 'units')
 
 
 class SessionSerializer(serializers.ModelSerializer):
+    sets = SetSerializer(read_only=True, many=True)
     class Meta:
         model = Session
         fields = ('created', 'title', 'sets')
--- workouts/urls.py
+++ workouts/urls.py
@@ -7,6 +7,17 @@ app_name='workouts'
 urlpatterns = [
     path('', views.SessionList.as_view(), name='SessionList'),
     re_path(r'^(?P<pk>[0-9]+)/$', views.SessionDetail.as_view(), name='SessionDetail'),
-    path('api/', api_views.SessionList.as_view(), name='SessionListAPI'),
-    re_path(r'^api/(?P<pk>[0-9]+)/$', api_views.SessionDetail.as_view(), name='SessionDetailAPI'),
+
+    # sessions
+    path('api/sessions/', api_views.SessionList.as_view(), name='SessionListAPI'),
+    re_path(r'^api/sessions/(?P<pk>[0-9]+)/$', api_views.SessionDetail.as_view(), name='SessionDetailAPI'),
+
+    #sets
+    path('api/sets/', api_views.SetList.as_view(), name='SetListAPI'),
+    re_path(r'^api/sets/(?P<pk>[0-9]+)/$', api_views.SetDetail.as_view(), name='SetDetailAPI'),
+
+    # exercises
+    path('api/exercises/', api_views.ExerciseList.as_view(), name='ExerciseListAPI'),
+    re_path(r'^api/exercises/(?P<pk>[0-9]+)/$', api_views.ExerciseDetail.as_view(), name='ExerciseDetailAPI'),
+
 ]
