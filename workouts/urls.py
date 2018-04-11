abe686a6a4a7ad4d58e1c2331da4a7af5811957c
currypat <currypat1985@gmail.com>
2018-04-11 18:57:49 +0200

    added comments

--- workouts/urls.py
+++ workouts/urls.py
@@ -5,8 +5,17 @@ from workouts import api_views
 app_name='workouts'
 
 urlpatterns = [
+
+    ############################
+    # normal django view urls
+    ############################
+
     path('', views.SessionList.as_view(), name='SessionList'),
     re_path(r'^(?P<pk>[0-9]+)/$', views.SessionDetail.as_view(), name='SessionDetail'),
+
+    ############################
+    # django rest framework urls
+    ############################
 
     # sessions
     path('api/sessions/', api_views.SessionList.as_view(), name='SessionListAPI'),
@@ -19,5 +28,9 @@ urlpatterns = [
     # exercises
     path('api/exercises/', api_views.ExerciseList.as_view(), name='ExerciseListAPI'),
     re_path(r'^api/exercises/(?P<pk>[0-9]+)/$', api_views.ExerciseDetail.as_view(), name='ExerciseDetailAPI'),
+
+    ############################
+    # graphql urls ?
+    ############################
 
 ]
