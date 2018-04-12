ec8c4430fcf38fd32f179ca5ed96c788fcce97ab
patcurry <currypat1985@gmail.com>
2018-04-12 09:08:28 +0200

    whatever

--- workouts/urls.py
+++ workouts/urls.py
@@ -7,6 +7,9 @@ app_name='workouts'
 urlpatterns = [
     path('', views.SessionList.as_view(), name='SessionList'),
     re_path(r'^(?P<pk>[0-9]+)/$', views.SessionDetail.as_view(), name='SessionDetail'),
+
+    ####
+    # Django Rest Framework 
     path('api/', api_views.SessionList.as_view(), name='SessionListAPI'),
     re_path(r'^api/(?P<pk>[0-9]+)/$', api_views.SessionDetail.as_view(), name='SessionDetailAPI'),
 ]
